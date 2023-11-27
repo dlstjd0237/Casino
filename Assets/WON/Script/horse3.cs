e9bf027c36da930d966b337fda45433e669f02f3
circle0707 <pwon1222@naver.com>
2023-11-28 03:33:05 +0900

    셋째가재입양

--- Assets/WON/Script/horse3.cs
+++ Assets/WON/Script/horse3.cs
@@ -1,9 +1,9 @@
-using UnityEngine;
 using System.Collections;
+using UnityEngine;
 
-public class horse1 : MonoBehaviour
+public class horse3 : MonoBehaviour
 {
-    //1º¸ 1~3.3cm
+    //3º¸ 7cm
     float sec;
     void Start()
     {
@@ -15,12 +15,11 @@ public class horse1 : MonoBehaviour
     {
         while (true)
         {
-            int movement = Random.Range(10, 33);
+            int movement = Random.Range(65, 70);
             transform.position += new Vector3(0, movement, 0) * Time.deltaTime;
             print(movement);
-            sec = Random.Range(0.4f, 0.6f);//0.4~0.6
+            sec = Random.Range(2f, 3f);//1~2
             yield return new WaitForSeconds(sec);
-        } 
+        }
     }
-
 }
